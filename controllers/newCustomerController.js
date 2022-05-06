6ae40dfc03ef716ca6b1757a0d7a2f810ea68319
Unni Ramachandran <unnipbvr@gmail.com>
2022-05-06 08:48:37 -0400

    more cors

--- controllers/newCustomerController.js
+++ controllers/newCustomerController.js
@@ -5,8 +5,7 @@ module.exports = {
     db.Customer.findAll({ order: [["customerName", "ASC"]] })
       .then(
         (dbModel) => (
-          res.set("Access-Control-Allow-Origin", "*"), 
-          res.json(dbModel)
+          res.set("Access-Control-Allow-Origin", "*"), res.json(dbModel)
         )
       )
       // .catch((err) => res.status(422).json(err));
@@ -19,24 +18,29 @@ module.exports = {
     console.log(req.params);
     db.Customer.findOne({
       where: { customerId: parseInt(req.params.customerId) },
-    })
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
+    }).then((dbModel) =>
+      (res.set("Access-Control-Allow-Origin", "*"), res.json(dbModel)).catch(
+        (err) => res.status(422).json(err)
+      )
+    );
   },
   create: function (req, res) {
-    db.Customer.create(req.body)
-      .then((dbModel) => res.json(dbModel))
-      // .catch((err) => console.log(err));
-      .catch((err) => {
-        console.log(err.parent);
-        res.status(400).json(err);
-      });
+    db.Customer.create(req.body).then((dbModel) =>
+      (res.set("Access-Control-Allow-Origin", "*"), res.json(dbModel))
+        // .catch((err) => console.log(err));
+        .catch((err) => {
+          console.log(err.parent);
+          res.status(400).json(err);
+        })
+    );
   },
   update: function (req, res) {
     db.Customer.update(req.body, {
       where: { customerId: req.params.customerId },
-    })
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
+    }).then((dbModel) =>
+      (res.set("Access-Control-Allow-Origin", "*"), res.json(dbModel)).catch(
+        (err) => res.status(422).json(err)
+      )
+    );
   },
 };
