f2714b094fd02487ba954c89e4a79164721f7bc8
Unni Ramachandran <unnipbvr@gmail.com>
2022-06-05 23:17:51 -0400

    Updated Invoice calc logic

--- controllers/calculateInvoiceAmountController.js
+++ controllers/calculateInvoiceAmountController.js
@@ -18,7 +18,7 @@ module.exports = {
       billFromDate: req.body.billFromDate,
       unitRatePerKg: req.body.unitRatePerKg,
     };
-    // await db.BillingSummary.create(BillingSummaryRecord);
+    console.log(BillingSummaryRecord);
 
     //Find all latex entries that needs to be updated
     let latexEntries = await db.LatexCollection.findAll({
@@ -31,17 +31,20 @@ module.exports = {
     });
 
     //Calculate and update invoice amount for each latex entry
-    latexEntries.forEach((entry) => {
-      entry.dataValues.unitRatePerKg = BillingSummaryRecord.unitRatePerKg;
-      entry.dataValues.totalAmount =
-        entry.dataValues.dryWeight * BillingSummaryRecord.unitRatePerKg;
-      // let status = db.LatexCollection.update(entry.dataValues, {
-      //   where: {
-      //     seqNumber: parseInt(entry.seqNumber),
-      //     customerId: parseInt(entry.customerId),
-      //   },
-      // });
-    });
+    //Refer https://gist.github.com/joeytwiddle/37d2085425c049629b80956d3c618971 to see why forEach would not work here
+    await Promise.all(
+      latexEntries.map(async (entry) => {
+        entry.dataValues.unitRatePerKg = BillingSummaryRecord.unitRatePerKg;
+        entry.dataValues.totalAmount =
+          entry.dataValues.dryWeight * BillingSummaryRecord.unitRatePerKg;
+        let statusLatex = await db.LatexCollection.update(entry.dataValues, {
+          where: {
+            seqNumber: parseInt(entry.seqNumber),
+            customerId: parseInt(entry.customerId),
+          },
+        });
+      })
+    );
 
     //Make Bill Generation Entries by customer
     let cashPaymentEntries = await db.LatexCollection.findAll({
@@ -59,20 +62,41 @@ module.exports = {
       },
     });
 
-    let status;
-    cashPaymentEntries.forEach((cashEntry) => {
-      (cashEntry.paymentDate = BillingSummaryRecord.billToDate),
+    let statusCash;
+    let totalBillAmount = 0;
+    await Promise.all(
+      cashPaymentEntries.map(async (cashEntry) => {
+        console.log(cashEntry);
+        (cashEntry.paymentDate = BillingSummaryRecord.billToDate),
         (cashEntry.paymentType = 1),
         (cashEntry.paymentNotes = "Bill Generated on ".concat(
           moment(BillingSummaryRecord.billToDate).format("DD/MM/YYYY")
-        ));
-       status = db.CashPayment.create(cashEntry);
-    });
+          ));
+          statusCash = await db.CashPayment.create(cashEntry);
+          totalBillAmount +=  cashEntry.totalAmount
+      })
+    );
+    BillingSummaryRecord.numberOfBills = cashPaymentEntries.length;
+    BillingSummaryRecord.totalBillAmount = totalBillAmount;
 
-    if (status) {
-      res.json("Success");
+    let statusBillSummary = await db.BillingSummary.create(
+      BillingSummaryRecord
+    );
+    if (statusBillSummary) {
+      console.log(BillingSummaryRecord);
+      res.set("Access-Control-Allow-Origin", "*"), res.json("Success");
     } else {
       res.status(422);
     }
+  },
+  options: function (req, res) {
+    res.set({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
+      "Content-Type": "application/json",
+    }),
+      res.json();
   },
 };
--- routes/api/calculateInvoiceAmountApi.js
+++ routes/api/calculateInvoiceAmountApi.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const calculateInvoiceAmountController = require("../../controllers/calculateInvoiceAmountController");
-router.route("/").post(calculateInvoiceAmountController.calculateInvoiceAmount);
-
+router
+  .route("/")
+  .post(calculateInvoiceAmountController.calculateInvoiceAmount)
+  .options(calculateInvoiceAmountController.options);
 module.exports = router;
