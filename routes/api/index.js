8c5045dee57b02a9f532870be8905ac839843859
Unni Ramachandran <unnipbvr@gmail.com>
2022-06-23 09:47:45 -0400

    New "Apply Rate" API

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,8 @@ const latexCollectionRoute = require("./latexCollectionApi");
 const cashPaymentRoute = require("./cashPaymentApi");
 const billingSummaryRoute = require("./billingSummaryApi");
 const invoiceGenerationRoute = require("./invoiceGenerationApi");
-const calculateInvoiceAmountRoute = require("./calculateInvoiceAmountApi")
+const calculateInvoiceAmountRoute = require("./applyRateApi")
+const applyRateRoute = require("./applyRateApi")
 const uploadRoute = require("./uploadApi")
 const printRoute = require("./printApi")
 
@@ -15,6 +16,7 @@ router.use("/cashPayment", cashPaymentRoute);
 router.use("/billingSummary", billingSummaryRoute);
 router.use("/invoiceGeneration", invoiceGenerationRoute);
 router.use("/calculateInvoiceAmount", calculateInvoiceAmountRoute)
+router.use("/applyRate", applyRateRoute)
 router.use("/upload", uploadRoute)
 router.use("/print", printRoute)
 
