52555c6a2476eb033f195eb0ea5e7fd976bdfd0b
Unni Ramachandran <unnipbvr@gmail.com>
2022-06-30 23:20:07 -0400

    Removed console logging DB parameters

--- database/models/index.js
+++ database/models/index.js
@@ -8,7 +8,6 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
 
-console.log(config)
 if (config.use_env_variable) {
   var sequelizeConnection = new Sequelize(process.env[config.use_env_variable]);
 } else {
