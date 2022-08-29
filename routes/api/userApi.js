497faa11042aad169c09ef3d9fabf7c22c2b9d5f
Unni Ramachandran <unnipbvr@gmail.com>
2022-08-28 23:13:04 -0400

    Initial passport setup

---
+++ routes/api/userApi.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const userController = require("../../controllers/userController");
+
+// Requiring our custom middleware for checking if a user is logged in
+const isAuthenticated = require("../../middleware/isAuthenticated");
+
+// Matches with "/api/user"
+router
+  .route("/")
+  .get(userController.findAll)
+  .post(userController.create);
+
+// Matches with "/api/user/:id"
+router
+  .route("/:id")
+  .get(userController.findById)
+  .put(userController.update)
+  .delete(isAuthenticated, userController.remove);
+
+module.exports = router;
