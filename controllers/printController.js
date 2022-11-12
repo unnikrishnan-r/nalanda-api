66432adc35269e8bb6c2b597b2cf83343d166323
Unni Ramachandran <unnipbvr@gmail.com>
2022-11-12 09:47:15 -0500

    Invoice File Name return

--- controllers/printController.js
+++ controllers/printController.js
@@ -8,16 +8,6 @@ var moment = require("moment");
 const aws = require("aws-sdk");
 const s3 = new aws.S3();
 
-async function getSingleFile(fileName){
-  console.log(`Beginning to get - ${fileName}`)
-  const s3GetParams ={
-    Bucket: process.env.S3_BUCKET_NAME,
-    Key: fileName,  
-  }
-  const getStatus = await s3.getObject(s3GetParams).promise();
-  return getStatus.Body;
-}
-
 module.exports = {
   print: function (req, res, next) {
     console.log("Received print req", req.body.billingDate);
@@ -59,18 +49,26 @@ module.exports = {
       await Promise.all(
         customerBills.map(async (customerBills, index) => {
           let billDate = moment(customerBills.dataValues.paymentDate)
-          .subtract(1,'days').format("DDMMYYYY")
-          let derivedFileName = customerBills.dataValues.customerId+"_"+billDate+".pdf"
-          fileNames.push(derivedFileName)
+            .subtract(1, "days")
+            .format("DDMMYYYY");
+
+          let derivedFileName =
+            "https://" +
+            process.env.S3_BUCKET_NAME +
+            "." +
+            process.env.S3_REGION_NAME +
+            "/" +
+            customerBills.dataValues.customerId +
+            "_" +
+            billDate +
+            ".pdf";
+            console.log(derivedFileName)
+          fileNames.push(derivedFileName);
         })
       );
-      console.log(fileNames)
-      const promises = [];
-      fileNames.forEach((file) => promises.push(getSingleFile(file)))
-      const loopStatus = await Promise.all(promises)
-      console.log("Got All Files")
+      console.log(fileNames);
       res.set("Access-Control-Allow-Origin", "*"),
-        res.status(200).json(loopStatus);
+        res.status(200).json(fileNames);
     }
   },
 };
